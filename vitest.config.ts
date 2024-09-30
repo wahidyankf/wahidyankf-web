38c05053a2b1ee8410c917f5bf57512b71d2ace9
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-30 22:34:51 +0700

    fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    include: ["**/*.{test,spec}.{js,ts,jsx,tsx}"],
     globals: true,
     setupFiles: ["./src/test/setup.ts"],
   },
