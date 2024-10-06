c57f833d0e003ec43231a2b48bab078ed4986362
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-10-06 15:17:52 +0700

    fix GTM ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
         </div>
       </body>
       <GoogleAnalytics gaId="G-0F62KPYGM3" />
-      <GoogleTagManager gtmId="GTM-0F62KPYGM3" />
+      <GoogleTagManager gtmId="G-0F62KPYGM3" />
     </html>
   );
 }
