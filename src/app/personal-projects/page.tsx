831a3d6b4dc1f7e24080e8b9ff91ace97329d4d5
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-10-20 16:32:40 +0700

    update navigation

--- src/app/personal-projects/page.tsx
+++ src/app/personal-projects/page.tsx
@@ -97,7 +97,7 @@ function ProjectsContent() {
   ]);
 
   return (
-    <div className="flex-grow max-w-4xl mx-auto">
+    <div className="flex-grow max-w-4xl mx-auto w-full">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-8 text-center text-yellow-400">
         Personal Projects
       </h1>
@@ -162,7 +162,7 @@ function ProjectsContent() {
 
 export default function Projects() {
   return (
-    <main className="min-h-screen bg-gray-900 text-green-400 p-4 sm:p-8 md:p-12 lg:p-16 flex flex-col lg:flex-row pb-20 lg:pb-0">
+    <main className="min-h-screen bg-gray-900 text-green-400 p-4 sm:p-8 md:p-12 lg:p-16 lg:ml-80 flex flex-col pb-20 lg:pb-0">
       <Navigation activePage="projects" />
       <Suspense fallback={<div>Loading...</div>}>
         <ProjectsContent />
